9bd327b1ea41248b969589ae0a9ff1b314f0d549
Nikolay Vasin <talam0nal@gmail.com>
2015-09-11 15:31:03 +0300

    Code linting

--- weekDsaysInRussia.php
+++ weekDsaysInRussia.php
@@ -47,111 +47,111 @@
 			return $this->russianMonthInPrepositional;
 	}
 
-	/*
-		Возвращает текущий месяц на русском языке в предложном падеже
-	*/
-	function currentRussianMonthInPrepositional() {
-		$month = date('n');
-		$array = $this->russianMonthInPrepositional();
-		return $array[$month];
-	}
-
-	/*
-		Возращает количество рабочих дней в текущем месяце
-	*/		
-	function numberOfWorkDaysInCurrentMonth() {
-		$array = $this->workAndWeekendDaysInThisMonth();
-		return $array[0];
-	}
-
-	/*
-		Возвращает количество рабочих часов в текущем месяце 
-	*/
-	function numberOfWorkingHoursInThisMonth() {
-		$array = $this->workAndWeekendDaysInThisMonth();
-		return $array[1];		
-	}
-
-	/*
-		Возращает текущий рабочий день в месяце
-	*/
-	function currentWorkingDay() {
-		$array = $this->workAndWeekendDaysInThisMonth();
-		return $array[2];
-	}
-
-	/*
-		Метод для расчёта количества рабочих дней в текущем месяце
-		Работает пока что только с "простыми" месяцами, в которых нет праздничных дней.
-		Для просчёта "сложных" месяцев будет использоваться API http://basicdata.ru/api/calend/
-		Возвращает одномерный простой массив
-		- 0 - количество рабочих дней в текущем месяце
-		- 1 - количество рабочих часов в текущем месяце
-		- 2 - текущий будний день в этом месяце
-	*/
-
-	function workAndWeekendDaysInThisMonth() {
-		//Общее количество дней в текущем месяце
-		$daysInTheMonth = date('t');
-
-		//Текущий день месяца
-		$currentDayInThisMonth = date('j');
-
-		//Текущий месяц
-		$currentMonth = date('n');
-
-		//Текуший год
-		$currentYear = date('Y');
-
-		//Счётчик для подсчёта рабочих дней в этом месяце
-		$numberOfWorkDaysInCurrentMonth = 1;
-
-		//Текущая дата в формате день.месяц.год (без ведущих нулей)
-		$currentDayString = $currentDayInThisMonth.".".$currentMonth.".".$currentYear;
-
-		//Таймштамп текущего дня
-		$timestampOfCurrentDay = strtotime($currentDayString);
-
-		//Пробегаем все дни текущего месяца в цикле
-		for ($day=0; $day < $daysInTheMonth; $day++) {
-			//Поскольку отчёт начинается с нуля, прибавляем ко дню единицу
-			$dayPlusOne = $day+1;
-
-			//Строковое представление даты
-			$dateString = $dayPlusOne.".".$currentMonth.".".$currentYear;
-
-			//Таймштапм 
-			$timestamp = strtotime($dateString);
-
-			//День недели в формате "Sat"
-			$dayOfWeek = date('D', $timestamp);
-
-			//Если это не выходной день, то добавляем к счётчику будних
-			//дней единицу
-			if (!in_array($dayOfWeek, $this->weekEndDays)) {
-				$numberOfWorkDaysInCurrentMonth++;
-			}
-
-			//Если текущий день совпадает с днём в текущей итерации,
-			//то вычисляем порядковый номер рабочего дня в этом месяце
-			if ($timestamp === $timestampOfCurrentDay) {
-				//Текущий рабочий день в этом месяце
-				$currentWorkingDay = $numberOfWorkDaysInCurrentMonth-1;
-			}
+		/*
+			Возвращает текущий месяц на русском языке в предложном падеже
+		*/
+		function currentRussianMonthInPrepositional() {
+			$month = date('n');
+			$array = $this->russianMonthInPrepositional();
+			return $array[$month];
 		}
 
-		//Количество рабочих дней в месяце
-		$numberOfWorkDaysInCurrentMonth = $numberOfWorkDaysInCurrentMonth-1;
+		/*
+			Возращает количество рабочих дней в текущем месяце
+		*/		
+		function numberOfWorkDaysInCurrentMonth() {
+			$array = $this->workAndWeekendDaysInThisMonth();
+			return $array[0];
+		}
 
-		//Количество рабочих часов в месяце
-		$numberOfWorkingHoursInThisMonth = $numberOfWorkDaysInCurrentMonth*8;
+		/*
+			Возвращает количество рабочих часов в текущем месяце 
+		*/
+		function numberOfWorkingHoursInThisMonth() {
+			$array = $this->workAndWeekendDaysInThisMonth();
+			return $array[1];		
+		}
 
-		return [
-			$numberOfWorkDaysInCurrentMonth,
-			$numberOfWorkingHoursInThisMonth,
-			$currentWorkingDay
-		];
-	}
+		/*
+			Возращает текущий рабочий день в месяце
+		*/
+		function currentWorkingDay() {
+			$array = $this->workAndWeekendDaysInThisMonth();
+			return $array[2];
+		}
+
+		/*
+			Метод для расчёта количества рабочих дней в текущем месяце
+			Работает пока что только с "простыми" месяцами, в которых нет праздничных дней.
+			Для просчёта "сложных" месяцев будет использоваться API http://basicdata.ru/api/calend/
+			Возвращает одномерный простой массив
+			- 0 - количество рабочих дней в текущем месяце
+			- 1 - количество рабочих часов в текущем месяце
+			- 2 - текущий будний день в этом месяце
+		*/
+
+		function workAndWeekendDaysInThisMonth() {
+			//Общее количество дней в текущем месяце
+			$daysInTheMonth = date('t');
+
+			//Текущий день месяца
+			$currentDayInThisMonth = date('j');
+
+			//Текущий месяц
+			$currentMonth = date('n');
+
+			//Текуший год
+			$currentYear = date('Y');
+
+			//Счётчик для подсчёта рабочих дней в этом месяце
+			$numberOfWorkDaysInCurrentMonth = 1;
+
+			//Текущая дата в формате день.месяц.год (без ведущих нулей)
+			$currentDayString = $currentDayInThisMonth.".".$currentMonth.".".$currentYear;
+
+			//Таймштамп текущего дня
+			$timestampOfCurrentDay = strtotime($currentDayString);
+
+			//Пробегаем все дни текущего месяца в цикле
+			for ($day=0; $day < $daysInTheMonth; $day++) {
+				//Поскольку отчёт начинается с нуля, прибавляем ко дню единицу
+				$dayPlusOne = $day+1;
+
+				//Строковое представление даты
+				$dateString = $dayPlusOne.".".$currentMonth.".".$currentYear;
+
+				//Таймштапм 
+				$timestamp = strtotime($dateString);
+
+				//День недели в формате "Sat"
+				$dayOfWeek = date('D', $timestamp);
+
+				//Если это не выходной день, то добавляем к счётчику будних
+				//дней единицу
+				if (!in_array($dayOfWeek, $this->weekEndDays)) {
+					$numberOfWorkDaysInCurrentMonth++;
+				}
+
+				//Если текущий день совпадает с днём в текущей итерации,
+				//то вычисляем порядковый номер рабочего дня в этом месяце
+				if ($timestamp === $timestampOfCurrentDay) {
+					//Текущий рабочий день в этом месяце
+					$currentWorkingDay = $numberOfWorkDaysInCurrentMonth-1;
+				}
+			}
+
+			//Количество рабочих дней в месяце
+			$numberOfWorkDaysInCurrentMonth = $numberOfWorkDaysInCurrentMonth-1;
+
+			//Количество рабочих часов в месяце
+			$numberOfWorkingHoursInThisMonth = $numberOfWorkDaysInCurrentMonth*8;
+
+			return [
+				$numberOfWorkDaysInCurrentMonth,
+				$numberOfWorkingHoursInThisMonth,
+				$currentWorkingDay
+			];
+		}
 
 	}
 
